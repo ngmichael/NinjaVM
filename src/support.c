fc424847147eeacc133e28b12ba6a3bcc0bfba79
Noah George Michael <noah.michael@mni.thm.de>
2017-12-11 08:55:51 +0100

    [WIP/DONTMERGE]: Started refactoring of project structure for bigint lib

--- src/support.c
+++ src/support.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include "headers/njvm.h"
 #include "headers/utils.h"
+#include "lib/support.h"
+
+
+/**
+ * Creates a new object instance on the heap
+ * and returns a pointer to the first byte of 
+ * that object. The size component of the object is
+ * set aswell.
+ * 
+ * @param size - the amount of bytes that the object shall hold
+ * @return A pointer to the first byte of this Object on the heap
+ */
+ObjRef allocate(unsigned int size) {
+    ObjRef object = calloc(size + sizeof(unsigned int), sizeof(unsigned int));
+    if (object == NULL) {
+        printf("Error: Failed to initialize memory for object with size %lu!\n", size + sizeof(unsigned int));
+        exit(E_ERR_SYS_MEM);
+    }
+    object->size = size;
+    return object;
+}
 
 /**
  * Changes the text color of the terminal to the
