c2e4580174dbf227f68204a60964f766c17109fb
Noah George Michael <noah.michael@mni.thm.de>
2018-01-02 21:46:43 +0100

    Implemented test case for instructions NEW, GETF, PUTF

---
+++ asm/src/objectTest.asm
@@ -0,0 +1,140 @@
+//
+// version
+//
+	.vers	7
+
+//
+// execution framework
+//
+__start:
+	call	_main
+	call	_exit
+__stop:
+	jmp	__stop
+
+//
+// Integer readInteger()
+//
+_readInteger:
+	asf	0
+	rdint
+	popr
+	rsf
+	ret
+
+//
+// void writeInteger(Integer)
+//
+_writeInteger:
+	asf	0
+	pushl	-3
+	wrint
+	rsf
+	ret
+
+//
+// Character readCharacter()
+//
+_readCharacter:
+	asf	0
+	rdchr
+	popr
+	rsf
+	ret
+
+//
+// void writeCharacter(Character)
+//
+_writeCharacter:
+	asf	0
+	pushl	-3
+	wrchr
+	rsf
+	ret
+
+//
+// Integer char2int(Character)
+//
+_char2int:
+	asf	0
+	pushl	-3
+	popr
+	rsf
+	ret
+
+//
+// Character int2char(Integer)
+//
+_int2char:
+	asf	0
+	pushl	-3
+	popr
+	rsf
+	ret
+
+//
+// void exit()
+//
+_exit:
+	asf	0
+	halt
+	rsf
+	ret
+
+//
+// void writeString(String)
+//
+_writeString:
+	asf	1
+	pushc	0
+	popl	0
+	jmp	_writeString_L2
+_writeString_L1:
+	pushl	-3
+	pushl	0
+	getfa
+	call	_writeCharacter
+	drop	1
+	pushl	0
+	pushc	1
+	add
+	popl	0
+_writeString_L2:
+	pushl	0
+	pushl	-3
+	getsz
+	lt
+	brt	_writeString_L1
+	rsf
+	ret
+
+//
+// void main()
+//
+_main:
+	asf	1
+	new	2
+	popg	0
+	pushg	0
+	call	_readInteger
+	pushr
+	putf	0
+	pushg	0
+	call	_readInteger
+	pushr
+	pushg	0
+	getf	0
+	add
+	putf	1
+	pushg	0
+	getf	0
+	pushg	0
+	getf	1
+	add
+	popl	0
+	pushl	0
+	call	_writeInteger
+	drop	1
+__0:
+	rsf
+	ret
