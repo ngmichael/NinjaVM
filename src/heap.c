67ef2ef538186cd4f5c6d5722a098bb3b1c8c4e0
Noah George Michael <noah.michael@mni.thm.de>
2018-01-06 02:58:44 +0100

    Added function doc to heap.c

--- src/heap.c
+++ src/heap.c
@@ -8,6 +8,9 @@ unsigned char* heap; /* Main Heap-Pointer */
 unsigned char* src;  /* Source pointer (Quell) - Unused*/
 unsigned char* dest; /* Destination pointer (Ziel) - Used for allocation*/
 
+/**
+ * Initializes the heap based on the value of variable heapSize
+ */
 void initHeap(void) {
     heap = calloc(heapSize, sizeof(unsigned char));
     if (heap == NULL) {
@@ -18,6 +21,12 @@ void initHeap(void) {
     src = heap + (heapSize / 2); 
 }
 
+/**
+ * Allocates memory for objects on the heap.
+ * 
+ * @param nBytes - the requested number of bytes of memory
+ * @return a pointer to the first byte of the requested memory
+ */
 unsigned char* allocate(unsigned int nBytes) {
     if (dest + nBytes <= src) {
         unsigned char* ret;
